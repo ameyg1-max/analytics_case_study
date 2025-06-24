5d34ab1a9a0d746a210bddae702235dc95b0ad33
Amey Ghodke <amey.g1@gmail.com>
2025-06-24 09:30:52 +0000

    fixes to dimensions

--- models/staging/stg_dim_station.sql
+++ models/staging/stg_dim_station.sql
@@ -3,26 +3,20 @@ with
 select
 distinct
 start_station_id as station_id,
-start_station_name as station_name,
-start_station_latitude as station_latitude,
-start_station_longitude as station_longitude
+start_station_name as station_name
 from
 {{ ref('lnd_citibike_trips') }}
 union 
 select 
 distinct
 end_station_id,
-end_station_name,
-end_station_latitude,
-end_station_longitude,
+end_station_name
 from
 {{ ref('lnd_citibike_trips') }})
 select 
-{{ dbt_utils.generate_surrogate_key(['station_id','station_name','station_latitude','station_longitude'])}} as dim_station_key,
+{{ dbt_utils.generate_surrogate_key(['station_id','station_name'])}} as dim_station_key,
 station_id,
 station_name,
-station_latitude,
-station_longitude
 from
 stations
 where station_id is not null
@@ -31,5 +25,3 @@ select
 '-1' as dim_station_key,
 -1 as station_id,
 'Unknown' as station_name,
--1 as station_latitude,
--1 as station_longitude
